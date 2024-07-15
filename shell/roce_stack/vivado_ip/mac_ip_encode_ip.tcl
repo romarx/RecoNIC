2db44e5c3bcbcc57c28864d1f84595542e410fba
Roman Marquart <maroman@student.ethz.ch>
2024-07-15 15:30:32 +0200

    update makefile and vivado ip

--- shell/roce_stack/vivado_ip/mac_ip_encode_ip.tcl
+++ shell/roce_stack/vivado_ip/mac_ip_encode_ip.tcl
@@ -1,3 +1,1 @@
-set_property  ip_repo_paths  ${root_dir}/shell/roce_stack/build/hls/mac_ip_encode/mac_ip_encode_prj [current_project]
-update_ip_catalog
 create_ip -name mac_ip_encode -vendor ethz.systems.fpga -library hls -version 2.0 -module_name mac_ip_encode_ip -dir ${ip_build_dir} 
